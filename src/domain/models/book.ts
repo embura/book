1aaa383efe6d04ed65b8a1f98ec3f7c69a599cf3
Jefferson S Coelho <jefferson.coelho@farmaciasapp.com.br>
2023-08-07 21:32:46 -0300

    feat: criando models  de book e bookRental

---
+++ src/domain/models/book.ts
@@ -0,0 +1,22 @@
+export namespace Book{
+
+    export interface Common {
+        title: string
+        author: string
+        gender: string
+        hasAudio: boolean
+        createdAt: Date
+        updatedAt: Date
+    }
+
+    export interface  WithId extends Common {
+        id: string
+    }
+
+    export interface Description extends  WithId{
+        description: string
+        stock: number
+        ISBN: string
+    }
+
+}
