ab32077d1a46dc88e97bd87bde0e0f00afd6a1e5
Jefferson S Coelho <jefferson.coelho@farmaciasapp.com.br>
2023-08-07 21:36:48 -0300

    fix: renamed books models

---
+++ src/domain/models/bookRental.ts
@@ -0,0 +1,12 @@
+export namespace BookRental{
+
+    export interface Common {
+        bookId: string
+        userId: string
+        startDate: Date
+        endDate: Date
+        createdAt: Date,
+        updatedAt: Date
+    }
+
+}
