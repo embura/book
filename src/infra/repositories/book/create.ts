14b223b2af8ddd1c995b03cdff33fdbbc22f2d27
Jefferson S Coelho <jefferson.coelho@farmaciasapp.com.br>
2023-08-10 22:46:23 -0300

    feat/createBook created createBook infra

---
+++ src/infra/repositories/book/create.ts
@@ -0,0 +1,13 @@
+import { Collection } from 'mongodb'
+
+import { CreateBook } from '@domain/repositories/book'
+
+export class MongoCreateBookRepository
+    implements CreateBook.Create {
+    constructor(private readonly collection: Collection<CreateBook.Input.CreateBookInput>) { }
+
+    async create(input: CreateBook.Input.CreateBookInput): Promise<void> {
+        await this.collection.insertOne(input)
+    }
+
+}
---
+++ src/infra/repositories/book/index.ts
@@ -0,0 +1,4 @@
+export * from './create'
+// export * from './get'
+// export * from './update'
+// export * from './delete'
