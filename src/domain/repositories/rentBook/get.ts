8843333c3561da0a2426ee84f196e2b07017327d
Jefferson S Coelho <jefferson.coelho@farmaciasapp.com.br>
2023-08-13 12:27:14 -0300

    feature/rentBook added rentBook repository

---
+++ src/domain/repositories/rentBook/get.ts
@@ -0,0 +1,14 @@
+import { RentBook as RentBookModel } from '@domain/models/rentBook'
+
+export namespace GetRentBook {
+  export namespace Input {
+    export type GetRentBookInput = {
+      bookId: string,
+      isRented: boolean,
+    }
+  }
+
+  export interface Get {
+    get(input: Input.GetRentBookInput): Promise<RentBookModel.WithId | null>
+  }
+}
