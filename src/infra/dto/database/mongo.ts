0cbd21f55ec56be81f376cfe1d92f8d90bcbf1a8
Jefferson S Coelho <jefferson.coelho@farmaciasapp.com.br>
2023-08-13 16:41:29 -0300

    feature/deleteBookUsecase create deleteBook dto

--- src/infra/dto/database/mongo.ts
+++ src/infra/dto/database/mongo.ts
@@ -4,15 +4,12 @@ import { z } from 'zod'
 export const mongoIdToString = (id: unknown) =>
   id instanceof ObjectId ? id.toString() : id
 
-export const idSchema = z.preprocess(
-  mongoIdToString,
-  z.string().refine((val) => {
-    return ObjectId.isValid(val)
-  })
-)
+export const idSchema = z.preprocess(mongoIdToString, z.string())
 
 export const objectIdSchema = z.object({
-  id: idSchema
+  id: z.string().refine((val) => {
+    return ObjectId.isValid(val)
+  })
 })
 
 export function arrayDefault<T>(schema: z.ZodType<T>) {
